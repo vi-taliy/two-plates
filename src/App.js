cc878c2cce23d98a6ba24f9be1087ea882a00644
vitaliy <vitaliy@vt.edu>
2025-03-05 16:25:55 -0500

    more routing fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './Home/HomePage';
 import MatchmakingPage from './Matchmaking/MatchmakingPage';
@@ -7,16 +7,15 @@ import ResultsPage from './Results/ResultsPage';
 
 function App() {
   return (
-    <BrowserRouter basename="/vi-taliy.github.io/two-plates">
+    <HashRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/matchmaking" element={<MatchmakingPage />} />
           <Route path="/results" element={<ResultsPage />} />
-          <Route path="*" element={<Navigate to="/" />} /> 
         </Routes>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
